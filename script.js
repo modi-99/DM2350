6cac4930b1f1b29fa967132d7d80c896733ea1e8
modi-99 <78117231+modi-99@users.noreply.github.com>
2023-07-23 20:38:04 +0200

    Commited

--- script.js
+++ script.js
@@ -1,76 +1,420 @@
+let images=['images/blueCircle.png',
+            'images/greenCircle.png',
+            'images/redCircle.png',
+            'images/yellowCircle.png'/*,
+            'images/blueTriangle.png',
+            'images/greenTriangle.png',
+            'images/redTriangle.jpg',
+            'images/yellowTriangle.png',
+            'images/file1.png',
+            'images/file2.png',
+            'images/file3.png',
+            'images/file4.png',
+            'images/spiral1.png',
+            'images/spiral2.png',
+            'images/spiral3.png',
+            'images/spiral4.png',
+            'images/roundrectangle1.png',
+            'images/roundrectangle2.png',
+            'images/roundrectangle3.png',
+            'images/roundrectangle4.png',
+            'images/bodypart 1.png',
+            'images/bodypart 2.png',
+            'images/bodypart 3.png',
+            'images/bodypart 4.png',
+            'images/cat 1.jpg',
+            'images/cat 2.jpg',
+            'images/cat 3.jfif',
+            'images/cat 4.jpg',
+            'images/flower emoji 1.png',
+            'images/flower emoji 2.png',
+            'images/flower emoji 3.png',
+            'images/flower emoji 4.png',
+            'images/number 1.png',
+            'images/number 2.png',
+            'images/number 3.png',
+            'images/number 4.png',
+            'images/weather 1.png',
+            'images/weather 2.png',
+            'images/weather 3.png',
+            'images/weather 4.png',
+            'images/arrow1.png',
+            'images/arrow2.png',
+            'images/arrow3.png',
+            'images/arrow4.png',
+            'images/pokemon1.png',
+            'images/pokemon2.png',
+            'images/pokemon3.png',
+            'images/pokemon4.png',
+            'images/black tshirt.jpg',
+            'images/blue tshirt.jpg',
+            'images/red tshirt.jpg',
+            'images/white tshirt.jpg',
+            'images/blueCircle.png',
+            'images/greenCircle.png',
+            'images/redCircle.png',
+            'images/yellowCircle.png',
+            'images/blueTriangle.png',
+            'images/greenTriangle.png',
+            'images/redTriangle.jpg',
+            'images/yellowTriangle.png',*/
+        ];
+        let answerImages=[
+            'images/blueCircle.png',
+            'images/redCircle.png',
+            'images/greenCircle.png',
+            'images/yellowCircle.png',
+            'images/greenTriangle.png',
+            'images/blueTriangle.png',
+            'images/yellowTriangle.png',
+            'images/redTriangle.jpg',
+            'images/file2.png',
+            'images/file1.png',
+            'images/file4.png',
+            'images/file3.png',
+            'images/spiral4.png',
+            'images/spiral2.png',
+            'images/spiral3.png',
+            'images/spiral1.png',
+            'images/roundrectangle4.png',
+            'images/roundrectangle1.png',
+            'images/roundrectangle3.png',
+            'images/roundrectangle2.png',
+            'images/bodypart 1.png',
+            'images/bodypart 2.png',
+            'images/bodypart 4.png',
+            'images/bodypart 3.png',
+            'images/cat 1.jpg',/*
+            'images/cat 3.jfif',
+            'images/cat 2.jpg',
+            'images/cat 4.jpg',
+            'images/flower emoji 3.png',
+            'images/flower emoji 2.png',
+            'images/flower emoji 1.png',
+            'images/flower emoji 4.png',
+            'images/number 1.png',
+            'images/number 3.png',
+            'images/number 2.png',
+            'images/number 4.png',
+            'images/weather 4.png',
+            'images/weather 1.png',
+            'images/weather 2.png',
+            'images/weather 3.png',
+            'images/arrow4.png',
+            'images/arrow3.png',
+            'images/arrow1.png',
+            'images/arrow2.png',
+            'images/pokemon2.png',
+            'images/pokemon1.png',
+            'images/pokemon3.png',
+            'images/pokemon4.png',
+            'images/white tshirt.jpg',
+            'images/blue tshirt.jpg',
+            'images/black tshirt.jpg',
+            'images/red tshirt.jpg',
+            'images/blueCircle.png',
+            'images/redCircle.png',
+            'images/greenCircle.png',
+            'images/yellowCircle.png',
+            'images/greenTriangle.png',
+            'images/blueTriangle.png',
+            'images/yellowTriangle.png',
+            'images/redTriangle.jpg'*/     
+        ];
+
+let oddImages=['images/bodypart 1.png',
+                'images/bodypart 4.png',
+                'images/bodypart 3.png',
+                'images/bodypart 2.png',
+                'images/cat 2.jpg',
+                'images/cat 1.jpg',
+                'images/cat 3.jfif',
+                'images/cat 4.jpg',
+                'images/flower emoji 4.png',
+                'images/flower emoji 2.png',
+                'images/flower emoji 3.png',
+                'images/flower emoji 1.png'/*,
+                'images/number 1.png',
+                'images/number 2.png',
+                'images/number 4.png',
+                'images/number 3.png',
+                'images/weather 4.png',
+                'images/weather 2.png',
+                'images/weather 3.png',
+                'images/weather 1.png',
+                'images/arrow2.png',
+                'images/arrow3.png',
+                'images/arrow4.png',
+                'images/pokemon1.png',
+                'images/arrow4.png',
+                'images/pokemon1.png',
+                'images/pokemon2.png',
+                'images/pokemon3.png',
+                'images/spiral2.png',
+                'images/spiral3.png',
+                'images/spiral4.png',
+                'images/roundrectangle1.png',
+                'images/spiral4.png',
+                'images/roundrectangle1.png',
+                'images/roundrectangle2.png',
+                'images/roundrectangle3.png',
+                'images/yellowTriangle.png',
+                'images/file1.png',
+                'images/file2.png',
+                'images/file3.png',
+                'images/file4.png',
+                'images/spiral1.png',
+                'images/spiral2.png',
+                'images/spiral3.png',
+                'images/greenTriangle.png',
+                'images/redTriangle.jpg',
+                'images/yellowTriangle.png',
+                'images/file1.png',
+                'images/yellowCircle.png',
+                'images/blueTriangle.png',
+                'images/greenTriangle.png',
+                'images/redTriangle.jpg',
+                'images/roundrectangle1.png',
+                'images/blueCircle.png',
+                'images/greenCircle.png',
+                'images/redCircle.png', 
+                'images/art1.jpg',
+                'images/art2.jpg',
+                'images/art3.jpg',
+                'images/art4.jpg',
+                'images/bear1.jpg',
+                'images/bear2.jpg',
+                'images/bear3.jpg',
+                'images/bear4.jpg',
+                'images/bird1.jpg',
+                'images/bird2.jpg',
+                'images/bird3.jpg',
+                'images/bird4.jpg',
+                'images/clav1.jpg',
+                'images/clav2.jpg',
+                'images/clav3.jpg',
+                'images/clav4.jpg',
+                'images/fish1.png',
+                'images/fish2.png',
+                'images/fish3.png',
+                'images/fish4.png',
+                'images/frog1.jpg',
+                'images/frog2.jpg',
+                'images/frog3.jpg',
+                'images/frog4.jpg',
+                'images/insect 4.png',
+                'images/insect 2.png',
+                'images/insect 3.png',
+                'images/insect 1.png',
+                'images/man.png',
+                'images/man4.png',
+                'images/man3.png',
+                'images/man2.png',
+                'images/tomato.jpg',
+                'images/tomato2.jpg',
+                'images/tomato5.jpg',
+                'images/tomato3.jpg'*/   
+            ]
 
 
 
 
+let imgTime=[];
+let submitTime=[];
 
-  const questions=[
-  "Very first question", 
-  "First question", 
-  "Second question", 
-  "Third question", 
-  "Four question", 
-  "thank you for answering",
-  "First question", 
-  "Second question", 
-  "Third question", 
-  "Four question"]; 
-  // print questions stored in array after pressing yes or no button.
-    let count = 0;
-    const element= document.getElementById("Qs");
-    const button1 = document.getElementById("increment");
-    const button2 = document.getElementById("decrement");
-    const textHolder = document.getElementById("counter");
-    
-    
-    //Setting up the timer
-    let sec=min=0;
+const popupDiv=document.getElementById("popup");
+popupDiv.style.visibility="hidden";
+const first =document.getElementById("firstAlternative");
+const second=document.getElementById("secondAlternative");
+const third=document.getElementById("thirdAlternative");
+const fourth=document.getElementById("fourthAlternative");
+const submitBtn=document.getElementById("submitAnswer");
+const element=document.getElementsByClassName("choosenImg");
+const cancelBtn=document.getElementById("cancel");
+let i=1;
+let b=0;
+let c=0;
+let x=1;
+ 
+
+
+let sec=min=0;
       
       let element2=setInterval(() => {
           if(sec!=99){
               sec++;
-              if(sec<10) secLab.innerHTML='0'+sec;
-              else secLab.innerHTML=sec;
+              //if(sec<10) secLab.innerHTML='0'+sec;
+              //else secLab.innerHTML=sec;
           }else{
               min++;
-              if(min<10) minLab.innerHTML='0'+min;
-              else minLab.innerHTML=min;
+              //if(min<10) minLab.innerHTML='0'+min;
+              //else minLab.innerHTML=min;
               sec=0;
           }
       },10);
-  // timer code finshes
 
 
 
+submitBtn.addEventListener("click", function(){
+    let audio= new Audio('ding1.mp3');
+    audio.volume=0.1;
+      audio.play();
+    document.body.style.backgroundColor="#ffffff";
+    popupDiv.style.visibility="hidden";
+    loggingInfo("submit ",submitTime);
+    x++;
+    if (i<images.length) {
+        document.getElementById("sourceImg").src=images[i];
+        i++;
+        b++;
+        if(b%4==0) changeMiniPics();   
+    }
+    else {
+        document.getElementById("sourceImg").style.visibility="hidden";
+        oddPics();
+        c+=4; 
+        if (c==oddImages.length+4) {
+            document.getElementById("question2").style.visibility="visible";
+            first.style.visibility="hidden";
+            second.style.visibility="hidden";
+            third.style.visibility="hidden";
+            fourth.style.visibility="hidden";
+            loadToTable();
+            exportToCSV();
+            alert("thank you for answering");
 
-  element.innerHTML=questions[count];
-  count++; 
-  //declaretion for call function
-  const secLab=document.getElementById('ms');
-  const minLab=document.getElementById('sec');
-    call(button1);
-    call(button2);
+        }
+         
+    }   
+})
 
+cancelBtn.addEventListener("click", function () {
+  popupDiv.style.visibility="hidden";
+  document.body.style.backgroundColor="#ffffff";
+  
+})
+
+
+function nextImg(){
+    let audio= new Audio('ding1.mp3');
+    audio.volume=0.1;
+    audio.play();
+    popupDiv.style.visibility="visible";
+    document.body.style.backgroundColor="rgba(135, 135, 135, 0.600)";
+
+    loggingInfo("image ",imgTime)
     
-    // functions 
-  function call(button) {
-    //adding a listner to make sure that the questions loop after each click.
-    button.addEventListener("click", function() {
-      element.innerHTML=questions[count];
-      count++;
-      textHolder.innerHTML = "Amount of questions Remaining: "+(questions.length-count);
-      console.log(minLab.innerHTML=min,':',secLab.innerHTML=sec)
+}
 
-      if(count>questions.length-1){
-        button1.style.visibility="hidden";
-        button2.style.visibility="hidden";
-        clearInterval(element2);
+function changeMiniPics(){
+    console.log("hello");
+    if (i<images.length) {
+        first.src=answerImages[i-1];
+        second.src=answerImages[i];
+        third.src=answerImages[i+1];
+        fourth.src=answerImages[i+2];
+    }
+
+}
+
+function oddPics(){
+    
+    first.src=oddImages[c];
+    second.src=oddImages[c+1];
+    third.src=oddImages[c+2];
+    fourth.src=oddImages[c+3]; 
+    if (true) {
+        document.getElementById("question2").style.visibility="visible";
+        document.getElementById("question1").style.visibility="hidden";
+    }
+}
+
+function loggingInfo(name,array) {
+    localStorage.setItem(name+[x],min+':'+sec);
+    array.push(min+':'+sec);
+}
+
+function loadToTable(){
+
+    const tablebody= document.getElementById("tableData");
+    let dataHtml=''; 
+    dataHtml=`<tr><td>${"NAME"}</td><td>${localStorage.getItem("NAME")}</td><td>-</td></tr>`;
+    tablebody.innerHTML=dataHtml;
+    dataHtml+=`<tr><td>${"AGE"}</td><td>${localStorage.getItem("AGE")}</td><td>-</td></tr>`;
+    tablebody.innerHTML=dataHtml;
+    dataHtml+=`<tr><td>${"ETHNICITY"}</td><td>${localStorage.getItem("ETHNICITY")}</td><td>-</td></tr>`;
+    tablebody.innerHTML=dataHtml;
+    let i=1;
+    let a=0;
+    
+    
+    for(let time of imgTime){
+      dataHtml+= `<tr><td>${"Question"+[i]}</td><td>${time}</td><td>${submitTime[a]}</td></tr>`;
+      tablebody.innerHTML=dataHtml;
+      i++;
+      a++;
+    }
+}
+
+function exportToCSV(){
+    const dataTable=document.getElementById("contentTable");
+    const exporter= new TableCSVExporter(dataTable);
+    const CSVOutput=exporter.convertToCSV();
+    const CSVBlob= new Blob([CSVOutput], {type: "text/csv"});
+    const BlobUrl= URL.createObjectURL(CSVBlob);
+    const anchorElement=document.createElement("a");
+    anchorElement.href=BlobUrl;
+    anchorElement.download="table-excel.csv";
+    anchorElement.click();
+    console.log(BlobUrl);
+    setTimeout(()=>{
+      URL.revokeObjectURL(BlobUrl);
+    }, 500)
+  }
+  
+  
+  
+  class TableCSVExporter{
+    constructor(table){
+      this.table=table; //declaring HTML table element
+      this.rows=Array.from(table.querySelectorAll("tr")); //initilaizing array for all tr elements
+      console.log(this);
+    }
+  
+    convertToCSV(){
+      const lines=[];
+      const numCols= 3;
+      for(const row of this.rows){
+        let line="";
+        for(let i=0; i<numCols;i++){
+          if (row.children[i]!==undefined) {
+            line+=TableCSVExporter.parseCell(row.children[i]);
+          }
+          line +=(i!==(numCols-1)) ? ",":"";
+        }
+        lines.push(line);
       }
-    });
+      return lines.join("\n");
+    }
+  
+    static parseCell(tableCell){
+      let parsedValue=tableCell.textContent;
+  
+      parsedValue= parsedValue.replace(/"/g,`""`);
+  
+      parsedValue=/[",\n]/.test(parsedValue) ? `"${parsedValue}"`:parsedValue;
+  
+      return parsedValue; 
+    }
+  }
 
-  };
-
-
-
-
-//CSS changes
-element.style.fontSize="30px";
+  function handleLocalStorage() {
+    const name=document.getElementById("name").value;
+    const age= document.getElementById("age").value;
+    const ethnicity=document.getElementById("ethnicity").value;
+  
+    localStorage.setItem("NAME", name);
+    localStorage.setItem("AGE", age);
+    localStorage.setItem("ETHNICITY", ethnicity);
+    return;
+  }
